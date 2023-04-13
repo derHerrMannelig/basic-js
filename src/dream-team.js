5d605517dd304bbceebe3dedd093fd80a2301a4a
derHerrMannelig <derherrmannelig@gmail.com>
2023-04-13 15:58:52 +0300

    feat: add rough implementation of dream-team solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  let format = [];
+  for (i = 0; i < members.length; i++) {
+    if (typeof members[i] !== 'string') {
+      continue
+    }
+    format.push(members[i]);
+  }
+  let letters = format.map((format) => format[0].toUpperCase());
+  return letters.sort().join('');
+
 }
 
 module.exports = {
