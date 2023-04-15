21dca43a72f8b2c0a18d5d65e95b38ca5894d0a2
derHerrMannelig <derherrmannelig@gmail.com>
2023-04-15 18:02:24 +0300

    fix: fix bugs in transform-array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -15,22 +15,42 @@ const { NotImplementedError } = require('../extensions/index.js');
  */
 function transform(arr) {
   trans = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === '--double-next') {
-      let dn = arr[i + 1];
-      trans.push(dn);
-    } else if (arr[i] === '--double-prev') {
-      let dp = arr[i - 1];
-      trans.push(dp);
-    } else if (arr[i] === '--discard-prev') {
-      trans.pop();
-    } else if (arr[i] === '--discard-next') {
-      i++;
-    } else {
-      trans.push(arr[i]);
+  if (Array.isArray(arr) === false) {
+    throw new Error("'arr' parameter must be an instance of the Array!")
+  } else {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === '--double-next') {
+        if (arr[i + 1] !== undefined) {
+          let dn = arr[i + 1];
+          trans.push(dn);
+        } else {
+          continue;
+        }
+      } else if (arr[i] === '--double-prev') {
+        if (arr[i - 1] !== undefined) {
+          let dp = arr[i - 1];
+          trans.push(dp);
+        } else {
+          continue;
+        }
+      } else if (arr[i] === '--discard-prev') {
+        if (arr[i - 1] !== undefined) {
+          trans.pop();
+        } else {
+          continue;
+        }
+      } else if (arr[i] === '--discard-next') {
+        if (arr[i + 1] !== undefined) {
+          i++;
+        } else {
+          continue;
+        }
+      } else {
+        trans.push(arr[i]);
+      }
     }
+    return trans;
   }
-  return trans;
 }
 
 module.exports = {
